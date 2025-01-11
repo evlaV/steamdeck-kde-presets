ace15b168e23b0b21d377d20f01dc53a96543291
David Edmundson <kde@davidedmundson.co.uk>
2025-01-11 23:52:25 +0000

    Only force ibus via env vars on X11
    On Wayland everything input methods are directed through the compositor
    including ibus. Having an IM module explicitliy set at a toolkit level
    can break that being loaded correctly.

--- etc/xdg/plasma-workspace/env/ibus.sh
+++ etc/xdg/plasma-workspace/env/ibus.sh
@@ -1,3 +1,5 @@
-export QT_IM_MODULE=ibus
-export GTK_IM_MODULE=ibus
-export XMODIFIERS= @ im = ibus
+if [ "$XDG_SESSION_TYPE" = "x11" ]; then
+    export QT_IM_MODULE=ibus
+    export GTK_IM_MODULE=ibus
+    export XMODIFIERS= @ im = ibus
+fi
