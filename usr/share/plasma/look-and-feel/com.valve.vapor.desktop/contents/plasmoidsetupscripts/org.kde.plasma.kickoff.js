74a5ac6bf3eeff1abe9a830553ada9eece296089
David Redondo <kde@david-redondo.de>
2025-03-04 16:37:09 +0100

    Only show deck branding on decks
    How this works a small plasma startup script writes a config file
    into the runtime directory with information if the device is a deck
    or not. That can be read by theme components later.
    The splash is separated out and is the default splash on the deck.
    A system service updates the default splash of the vapor global theme.

--- usr/share/plasma/look-and-feel/com.valve.vapor.desktop/contents/plasmoidsetupscripts/org.kde.plasma.kickoff.js
+++ usr/share/plasma/look-and-feel/com.valve.vapor.desktop/contents/plasmoidsetupscripts/org.kde.plasma.kickoff.js
@@ -1,3 +1,6 @@
-applet.currentConfigGroup = ["General"]
-applet.writeConfig("icon", "distributor-logo-steamdeck")
-applet.reloadConfig()
+const config = ConfigFile("/run/user/1000/plasma-is-deck", "Device")
+if (config.readEntry("isDeck", false)) {
+    applet.currentConfigGroup = ["General"]
+    applet.writeConfig("icon", "distributor-logo-steamdeck")
+    applet.reloadConfig()
+}
