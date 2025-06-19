8a7859107df0a1f42cc2115994df13af1bf080fb
David Edmundson <kde@davidedmundson.co.uk>
2025-06-19 13:10:00 +0100

    Make Return To Gaming Mode icon forcefully go to gamescope
    The previous Return To Gaming mode icon performed a logout. This was
    sufficient at the time because SteamOS had a hardcoded default. We now
    want to cover the use case of defaulting to Plasma and being able to
    enter gaming mode. The new script makes use of new steamos-manager API
    to perform the switch.
    
    As the file may already exist on the user's desktop, it is synced from
    the base system on startup.
    
    The symlink line is changed so we don't bail the script out early if the
    link exists.

--- etc/xdg/plasma-workspace/env/set-return-icon.sh
+++ etc/xdg/plasma-workspace/env/set-return-icon.sh
@@ -4,5 +4,11 @@ board_name=$(cat /sys/class/dmi/id/board_name)
 if [[ "$board_name" == "Jupiter" || "$board_name" == "Galileo" ]] then
     destination="${XDG_DATA_HOME:-$HOME/.local/share}/icons/hicolor/scalable/actions"
     mkdir -p $destination
-    ln -s /usr/share/icons/hicolor/scalable/actions/steamdeck-gaming-return.svg $destination/gaming-return.svg
+    ln -sf /usr/share/icons/hicolor/scalable/actions/steamdeck-gaming-return.svg $destination/gaming-return.svg
+fi
+
+# Sync latest changes from upstream
+
+if [ -f "$HOME/Desktop/Return.desktop" ]; then
+    cp /etc/skel/Desktop/Return.desktop "$HOME/Desktop/Return.desktop"
 fi
